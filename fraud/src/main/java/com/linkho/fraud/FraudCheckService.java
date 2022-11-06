e8250532619ef25efb42d8fe52cd9e21a7cbad08
risethesummer <honhatlinhmail@gmail.com>
2022-11-06 14:44:37 +0700

    add service classes

---
+++ fraud/src/main/java/com/linkho/fraud/FraudCheckService.java
@@ -0,0 +1,12 @@
+package com.linkho.fraud;
+
+/**
+ * com.linkho.fraud
+ * Created by NhatLinh - 19127652
+ * Date 11/6/2022 - 2:41 PM
+ * Description: ...
+ */
+public interface FraudCheckService {
+    boolean isFraudulentCustomer(Integer customerId);
+}
+
