333d214259f8f0493c6a9d878f3d13a47962a579
risethesummer <honhatlinhmail@gmail.com>
2022-11-06 19:46:53 +0700

    config eureka client

--- customer/src/main/java/com/microservice_demo/customer/CustomerApplication.java
+++ customer/src/main/java/com/microservice_demo/customer/CustomerApplication.java
@@ -2,6 +2,7 @@ package com.microservice_demo.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * com.microservice_demo.customer
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Description: ...
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
