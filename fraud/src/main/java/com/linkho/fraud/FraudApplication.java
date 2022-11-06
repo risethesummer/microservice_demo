68327ac71b16a05d4e1035e1dd192cc9c938ec00
risethesummer <honhatlinhmail@gmail.com>
2022-11-06 14:08:21 +0700

    initial fraud service

---
+++ fraud/src/main/java/com/linkho/fraud/FraudApplication.java
@@ -0,0 +1,17 @@
+package com.linkho.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.linkho.fraud
+ * Created by NhatLinh - 19127652
+ * Date 11/6/2022 - 2:04 PM
+ * Description: ...
+ */
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
